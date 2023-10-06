1f03474cce932f7bf6f6a15693d09b0e8f4f7390
shawntwjohnson <138104240+shawntwjohnson@users.noreply.github.com>
2023-10-06 16:58:43 -0500

    Updated server.js and added MongoDB integration

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// MongoDB connection string
+// Replace with your actual connection string from MongoDB Atlas
+const MONGODB_URI = 'mongodb+srv://shawntwj:Sunshine1234!@cluster0.7ogqw4g.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname, '.')));
@@ -19,3 +39,13 @@ app.get('/calculator', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Define the schema for the job logs
+const jobLogSchema = new mongoose.Schema({
+    jobNumber: String,
+    totalLights: String,
+    lightType: String
+});
+
+// Create a model based on the schema
+const JobLog = mongoose.model('JobLog', jobLogSchema);
